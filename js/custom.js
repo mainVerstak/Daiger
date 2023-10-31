235a1850cb391f2bb4e84172e8ade82f86ecee6e
mainVerstak <54151544+mainVerstak@users.noreply.github.com>
2023-10-31 08:45:35 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,66 @@
+"use strict"
+//==================================================================================================================================================
+//Бэграунд картинок - "Начало"
+//==================================================================================================================================================
+
+function ibg() {
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+ibg();
+//==================================================================================================================================================
+//Бэграунд картинок - "Конец"
+//==================================================================================================================================================
+
+
+
+
+
+//==================================================================================================================================================
+//Линивая загрусска - "Начало"
+//==================================================================================================================================================
+const lazyImages = document.querySelectorAll('img[data-src], source[data-srcset]');
+const loadMapBlock = document.querySelector('._load-map');
+const windowHeight = document.documentElement.clientHeight;
+
+let lazyImagesPositions = [];
+if (lazyImages.length > 0) {
+	lazyImages.forEach(img => {
+		if (img.dataset.src || img.dataset.srcset) {
+			lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset);
+			lazyScrollCheck();
+			ibg();
+		}
+	});
+}
+
+window.addEventListener("scroll", lazuScroll);
+function lazuScroll() {
+	if (document.querySelectorAll('img[data-src], source[data-srcset]').length > 0) {
+		lazyScrollCheck();
+		ibg();
+	}
+}
+
+function lazyScrollCheck() {
+	let imgIndex = lazyImagesPositions.findIndex(
+		item => pageYOffset > item - windowHeight
+	);
+	if (imgIndex >= 0) {
+		if (lazyImages[imgIndex].dataset.src) {
+			lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+			lazyImages[imgIndex].removeAttribute('data-src');
+		} else if (lazyImages[imgIndex].dataset.srcset) {
+			lazyImages[imgIndex].srcset = lazyImages[imgIndex].dataset.srcset;
+			lazyImages[imgIndex].removeAttribute('data-srcset');
+		}
+		delete lazyImagesPositions[imgIndex];
+	}
+}
+//==================================================================================================================================================
+//Линивая загрусска - "Начало"
+//==================================================================================================================================================
