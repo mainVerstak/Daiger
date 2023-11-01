a38bd3f8ee684bd56d3cf93a5074350da99f5b7f
mainVerstak <54151544+mainVerstak@users.noreply.github.com>
2023-11-01 08:46:24 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-"use strict"
+
 //==================================================================================================================================================
 //Бэграунд картинок - "Начало"
 //==================================================================================================================================================
@@ -14,6 +14,111 @@ function ibg() {
 ibg();
 //==================================================================================================================================================
 //Бэграунд картинок - "Конец"
+//==================================================================================================================================================
+
+
+
+
+
+//==================================================================================================================================================
+//Валидация формы - "Начало"
+//==================================================================================================================================================
+
+document.addEventListener("DOMContentLoaded", function () {
+	const forms = document.querySelectorAll("._form");
+
+	for (let i = 0; i < forms.length; i++) {
+		let form = forms[i];
+
+		form.addEventListener("submit", function (e) {
+			e.preventDefault();
+			let error = formValidate(form);
+
+			if (error === 0) {
+				form.submit();
+			}
+		});
+
+		let formFeqInputs = form.querySelectorAll("._req");
+
+		for (let i = 0; i < formFeqInputs.length; i++) {
+			let formFeqInput = formFeqInputs[i];
+
+			formFeqInput.parentElement.addEventListener('click', (e) => {
+				for (let i = 0; i < formFeqInputs.length; i++) {
+					let formFeqInput = formFeqInputs[i];
+					if (formFeqInput.classList.contains('_error')) {
+						formRemoveError(formFeqInput);
+					}
+				}
+			})
+		}
+
+		function formValidate(form) {
+			let error = 0;
+			let formFeq = form.querySelectorAll("._req");
+
+			for (var i = 0; i < formFeq.length; i++) {
+				let input = formFeq[i];
+				formRemoveError(input);
+
+				if (input.classList.contains('_email')) {
+					if (emailTest(input)) {
+						formAddError(input);
+						error++;
+					}
+				} else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+					formAddError(input);
+					error++;
+				} else if (input.getAttribute("type") === "tel" && input.value != '') {
+					if (!nomerTest(input.value)) {
+						formAddError(input);
+						error++
+					}
+				} else if (input.value === '') {
+					formAddError(input);
+					error++;
+				}
+			}
+
+			return error;
+		}
+
+		function formAddError(input) {
+			input.parentElement.classList.add("_error");
+			input.classList.add("_error");
+		}
+		function formRemoveError(input) {
+			input.parentElement.classList.remove("_error");
+			input.classList.remove("_error");
+		}
+		function emailTest(input) {
+			return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+		}
+		function nomerTest(nomer) {
+			if (true) {
+				if (nomer[0] === "8" && nomer.length == 17) {
+					return true;
+				} else if (nomer[0] === "+" && nomerTestSimvol(nomer) === "7" && nomer.length > 17) {
+					return true;
+				}
+			}
+		}
+		function nomerTestSimvol(nomer) {
+			for (let i = 1; i < nomer.length; i++) {
+				let simvol = nomer[i];
+
+				if (+simvol > 0) {
+					return simvol;
+				}
+			}
+		}
+	}
+});
+
+
+//==================================================================================================================================================
+//Валидация формы - "Конец"
 //==================================================================================================================================================
 
 
@@ -63,4 +168,107 @@ function lazyScrollCheck() {
 }
 //==================================================================================================================================================
 //Линивая загрусска - "Начало"
+//==================================================================================================================================================
+
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 1;
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				/*for (let index2 = 0; index2 < animItems.length; index2++) {
+					let animItem2 = animItems[index2];
+					animItem2.classList.remove('_active');
+				}*/
+				//animItem.style.height = animItem.clientHeight + "px";
+
+				animItem.classList.add('_active');
+			} else {
+				animItem.classList.remove('_active');
+			}
+		}
+
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollleft = window.pageXOffset || document.documentElement.scrollleft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollleft }
+	}
+	animOnScroll();
+}
+
+
+
+/*
+const body = document.body;
+const scrollWrap = document.getElementsByClassName("wrapper")[0];
+const height = scrollWrap.getBoundingClientRect().height - 1;
+const speed = 0.03;
+
+var offset = 0;
+
+function smoothScroll() {
+	offset += (window.pageYOffset - offset) * speed;
+
+	var scroll = "translateY(-" + offset + "px) translateZ(0)";
+	scrollWrap.style.transform = scroll;
+
+	callScroll = requestAnimationFrame(smoothScroll);
+}
+
+smoothScroll();*/
+/*
+window.onload = function () {
+	if (window.addEventListener) window.addEventListener("DOMMouseScroll", mouse_wheel, false);
+	window.onmousewheel = document.onmousewheel = mouse_wheel;
+}
+var mouse_wheel = function (event) {
+	if (false == !!event) event = window.event;
+	var direction = ((event.wheelDelta) ? event.wheelDelta / 120 : event.detail / -3) || false;
+}
+*/
+
+
+//==================================================================================================================================================
+//Копирование ссылок - "Начало"
+//==================================================================================================================================================
+
+const copiaContents = document.querySelectorAll('._copia-content');
+
+if (copiaContents) {
+	for (let i = 0; i < copiaContents.length; i++) {
+		let copiaContent = copiaContents[i];
+		let copiaContentButton = copiaContent.querySelector('._copia-content__button');
+		let copiaContentWrapper = copiaContent.querySelector('._copia-content__content');
+
+		copiaContentButton.addEventListener("click", function (e) {
+			if (copiaContentWrapper.classList.contains('_input')) {
+				let copiaContentWrapperContent = copiaContentWrapper;
+				navigator.clipboard.writeText(copiaContentWrapperContent.value);
+			} else {
+				let copiaContentWrapperContent = copiaContentWrapper.innerText;
+				navigator.clipboard.writeText(copiaContentWrapperContent);
+				copiaContent.classList.add('_active');
+
+				let timerinCopiaContent = setTimeout(function tick() {
+					copiaContent.classList.remove('_active');
+				}, 5000);
+			}
+		});
+	}
+}
+
+//==================================================================================================================================================
+//Копирование ссылок - "Конец"
 //==================================================================================================================================================
